6804c634f00ff12a3c33bb1b4ab59888e54092d4
trisston <trisstonprogramming@gmail.com>
2024-03-29 22:18:43 -0500

    Add Dropdown Navigation Logic

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ import { loadAddCatPage } from "./display";
 const navAll = document.getElementById("navAll");
 const navAddTask = document.getElementById("navAddTask");
 const navAddCat = document.getElementById("navAddCat");
+const dropdown = document.getElementById("select-dropdown");
 
 navAll.addEventListener("click", loadAllTasksPage);
 navAddTask.addEventListener("click", loadAddTaskPage);
 navAddCat.addEventListener("click", loadAddCatPage);
+dropdown.addEventListener("change", () => {
+    switch(dropdown.value){
+        case "addTask":
+            loadAddTaskPage();
+            break;
+        case "all":
+            loadAllTasksPage();
+            break;
+        case "addCat":
+            loadAddCatPage();
+        default:
+            break;
+    }
+})
